966c5a8759624adcc3f6a7905d7978bfcf1a46df
Kelvin Bush <kelybush@gmail.com>
2022-07-19 18:56:34 +0300

    fix parameter order

--- lib/school/student.rb
+++ lib/school/student.rb
@@ -1,1 +1,1 @@
-require_relative 'person'class Student < Person  def initialize(age, classroom, name = "Unknown", parent_permission = true)    super(age, name, parent_permission)    @classroom = classroom  end  def play_hooky    "¯\\(ツ)/¯"  endend
+require_relative 'person'class Student < Person  def initialize(classroom, age, name = "Unknown", parent_permission = true)    super(age, name, parent_permission)    @classroom = classroom  end  def play_hooky    "¯\\(ツ)/¯"  endend
