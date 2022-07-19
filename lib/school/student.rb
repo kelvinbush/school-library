f0ef0fa6cf02de7b6a1d0e706be7bf8db2ed427c
Kelvin Bush <kelybush@gmail.com>
2022-07-19 18:52:45 +0300

    create student class

---
+++ lib/school/student.rb
@@ -0,0 +1,1 @@
+require_relative 'person'class Student < Person  def initialize(age, name = "Unknown", parent_permission = true, classroom)    super(age, name, parent_permission)    @classroom = classroom  end  def play_hooky    "¯\\(ツ)/¯"  endend
---
+++ lib/school/teacher.rb
@@ -0,0 +1,1 @@
+class Teacherend
