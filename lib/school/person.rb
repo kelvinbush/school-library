55556986a8cbee9b166a45f08aa3c7221d235f17
Kelvin Bush <kelybush@gmail.com>
2022-07-19 18:48:24 +0300

    create person class

---
+++ lib/school/person.rb
@@ -0,0 +1,1 @@
+class Person  attr_accessor :name, :age  attr_reader :id  def initialize(age, name = "Unknown", parent_permission = true)    @age = age    @name = name    @parent_permission = parent_permission  end  def is_of_age?    @age >= 18  end  def can_use_services?    is_of_age? || @parent_permission  end  private :is_of_age?end
