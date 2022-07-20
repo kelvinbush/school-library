d4e89f2fa003c74af73e1a644d80525eb24c9a22
Kelvin Bush <kelybush@gmail.com>
2022-07-20 12:03:51 +0300

    add relations to person and student class

--- lib/school/student.rb
+++ lib/school/student.rb
@@ -1,12 +1,18 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+  attr_reader :classroom
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    @classroom = classroom
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
