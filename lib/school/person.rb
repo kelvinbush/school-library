5d47def4a2c610cd491a14ca78e0b55f3c0ff2be
Kelvin Bush <kelybush@gmail.com>
2022-07-20 05:36:56 +0300

    inherit nameble in person

--- lib/school/person.rb
+++ lib/school/person.rb
@@ -1,4 +1,6 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
@@ -15,6 +17,10 @@ class Person
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private :of_age?
