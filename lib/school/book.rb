1cc4b737595b5ccc17f57c30bcc7d0c67835a247
Kelvin Bush <kelybush@gmail.com>
2022-07-20 12:04:47 +0300

    clean up

--- lib/school/book.rb
+++ lib/school/book.rb
@@ -1,1 +1,10 @@
-class Book  attr_reader :rentals  attr_accessor :title, :author  def initialize(title, author)    @author = author    @title = title    @rentals = []  endend
+class Book
+  attr_reader :rentals
+  attr_accessor :title, :author
+
+  def initialize(title, author)
+    @author = author
+    @title = title
+    @rentals = []
+  end
+end
